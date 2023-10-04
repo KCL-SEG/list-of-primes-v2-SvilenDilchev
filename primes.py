7ae50b9d994a0c596dc7afdc0705389d8c0c6d2c
Svilen Dilchev <svilen.dilchev@kcl.ac.uk>
2023-10-04 21:38:29 +0300

    Add a prime generator with a value error exception for non-positive values

--- primes.py
+++ primes.py
@@ -3,4 +3,23 @@
 
 def primes(number_of_primes):
     list = []
+
+    if number_of_primes <= 0:
+        raise ValueError
+
+    if number_of_primes == 1:
+        return [2]
+
+    counter = 2
+    while len(list) < number_of_primes:
+
+        isPrime = True
+        for prime in list:
+            if counter % prime == 0:
+                isPrime = False
+
+        if isPrime:
+            list.append(counter)
+        counter += 1
+
     return list
